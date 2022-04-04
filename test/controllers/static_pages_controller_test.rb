8ad225a9f91787aae1d68b3a8d8bf07701ccbf3d
Raj Kiran Kaur <kiran.kaur@katalyst.com.au>
2022-04-04 13:52:26 +0930

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,14 +9,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title" , "Help | Raj Kiran Kaur"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title" , "About | Raj Kiran Kaur"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title" , "Contact | Raj Kiran Kaur"
   end
 end
