c9a79b9372d5722f84e8fc3b6a28f9f51f8ad1cb
Raj Kiran Kaur <kiran.kaur@katalyst.com.au>
2022-04-14 14:19:26 +0930

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-
-
-  # get 'static_pages/about'
-  # get 'static_pages/contact'
-
-
   root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users
 
 end
